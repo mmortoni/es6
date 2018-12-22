f147dffb582519e9c9a027ef51942ce72338b14b
Mauro Cardoso Mortoni <mmortoni@hotmail.com>
2018-12-22 18:05:53 -0200

    update TypeScriptStart

---
+++ app/playground.js
@@ -0,0 +1,15 @@
+var myString = 'abc';
+var myBoolean = true;
+var myNumber = 10.5;
+var myArray = ['A', 'B', 'C'];
+var myTuple = ['A', 10];
+var a = null;
+console.log(typeof a);
+console.log(a);
+/*
+console.log(myString);
+console.log(myBoolean);
+console.log(myNumber);
+console.log(myArray);
+console.log(myTuple);
+*/
---
+++ app/playground.ts
@@ -0,0 +1,17 @@
+const myString: string = 'abc';
+const myBoolean: boolean = true;
+const myNumber: number = 10.5;
+const myArray: string[] = ['A', 'B', 'C'];
+const myTuple: [string, number] = ['A', 10];
+
+const a: string = null;
+console.log(typeof a);
+console.log(a);
+
+/*
+console.log(myString);
+console.log(myBoolean);
+console.log(myNumber);
+console.log(myArray);
+console.log(myTuple);
+*/

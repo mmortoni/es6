f2e7b4583b85f72c4ab828534f0bc1baf5426bc7
Mauro Cardoso Mortoni <mmortoni@hotmail.com>
2018-12-26 21:07:28 -0200

    update Decorators

--- app/model/message.model.js
+++ app/model/message.model.js
@@ -1,25 +1,39 @@
-export default class Message {
-  constructor(text = '', created = Date.now()) {
-    this.text = text;
-    this.created = created;
-  }
-  get created() {
-    return this._created;
-  }
-  set created(created) {
-    if (typeof created === 'undefined' || isNaN(created)) {
-      throw new Error('Invalid created');
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var test = function (data) {
+    console.log(data);
+    return function (target) {
+        console.log(target);
+    };
+};
+var deprecated = function (target) {
+    console.log('This has been deprecated!');
+};
+var Message = /** @class */ (function () {
+    function Message(text, created) {
+        if (text === void 0) { text = ''; }
+        if (created === void 0) { created = Date.now(); }
+        this.text = text;
+        this.created = created;
     }
-    if (Message.hasOwnProperty.call(this, '_created')) {
-      throw new Error('Created already defined');
-    }
-    this._created = created;
-  }
-  toString() {
-    const { created, text } = this;
-    return `Message created at: ${created} - Text: ${text}`;
-  }
-  static newEmptyMessage() {
-    return new Message();
-  }
-}
+    Message_1 = Message;
+    Message.newEmptyMessage = function () {
+        return new Message_1();
+    };
+    Message.prototype.toString = function () {
+        var _a = this, created = _a.created, text = _a.text;
+        return "Message created at: " + created + " - Text: " + text;
+    };
+    var Message_1;
+    Message = Message_1 = __decorate([
+        test({ foo: 'bar' }), deprecated
+    ], Message);
+    return Message;
+}());
+exports.Message = Message;
